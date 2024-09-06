d8c2cba703af5959d716a9125b0202995700ae03
selmanbaskaya <selmanbaskaya1@gmail.com>
2024-09-06 11:25:43 +0300

    update gitignore file

--- Backend/obj/Debug/net8.0/backend.AssemblyInfo.cs
+++ Backend/obj/Debug/net8.0/backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+69d8134226afafae15d981738cf9f1fb6bcd737c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3cdde20f23e3960959ae858bfd7ceb1ea7dbc4dd")]
 [assembly: System.Reflection.AssemblyProductAttribute("backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
